6728630af37f17ddbb9e0470ca1e0abcaeb95d3d
abdulkareem <almalkias@icloud.com>
2023-05-06 03:07:24 +0300

    add my name

--- Lesson-2/0_HelloServer/HelloServer.py
+++ Lesson-2/0_HelloServer/HelloServer.py
@@ -23,3 +23,6 @@ if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
     httpd = HTTPServer(server_address, HelloHandler)
     httpd.serve_forever()
+
+
+print('abdulkareem-work')
